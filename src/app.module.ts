51e70e7b8a240b3786e7beff24048d8872249863
Italo Guajardo <italog.penaloza@gmail.com>
2022-04-01 18:42:46 -0300

    Create connection for mongodb, and waste docker-compose

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,24 @@
+import * as dotenv from 'dotenv';
+dotenv.config();
+
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { EventsController } from './events.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { join } from 'path';
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'mongodb',
+      useUnifiedTopology: true,
+      url: process.env.DB_HOST,
+      entities: [join(__dirname, '**/**.entity{.ts,.js}')],
+      synchronize: true,
+      logging: false,
+    }),
+  ],
   controllers: [AppController, EventsController],
   providers: [AppService],
 })
--- src/events.controller.ts
+++ src/events.controller.ts
@@ -9,38 +9,57 @@ import {
   Post,
 } from '@nestjs/common';
 import { CreateEventDto } from './create-event.dto';
+import { Event } from './event.entity';
 import { UpdatedEventDto } from './update-event.dto';
 
 @Controller('/events')
 export class EventsController {
+  private events: Event[] = [];
+
   // Ideal tener 5 acciones máximo
   // Esto ya que se debe trabajar con recursos tipo RESTful
   @Get()
   findAll() {
-    return [
-      { id: 1, name: 'First Event' },
-      { id: 2, name: 'Second Event' },
-    ];
+    return this.events;
   }
 
   @Get(':id')
-  findOne(@Param('id') id: string) {
-    return { id, name: 'First Event' };
+  findOne(@Param('id') id: string): Event {
+    const event = this.events.find((event) => event.id === Number(id));
+    return event;
   }
 
   // best practice for post and update is to return the value created/updated
   @Post()
-  create(@Body() input: CreateEventDto) {
-    return input;
+  create(@Body() input: CreateEventDto): Event {
+    const event = {
+      ...input,
+      when: new Date(input.when),
+      id: this.events.length + 1,
+    };
+    this.events.push(event);
+    return event;
   }
 
   @Patch(':id')
-  update(@Param('id') id: string, @Body() input: UpdatedEventDto) {
-    return input;
+  update(
+    @Param('id') id: string,
+    @Body() input: UpdatedEventDto,
+  ): Event | undefined {
+    const index = this.events.findIndex((event) => event.id === Number(id));
+
+    this.events[index] = {
+      ...this.events[index],
+      ...input,
+      when: input.when ? new Date(input.when) : this.events[index].when,
+    };
+    return this.events[index];
   }
 
   // best practice for delete is to return nothing but status code 204
   @Delete(':id')
   @HttpCode(204)
-  remove(@Param('id') id: string) {}
+  remove(@Param('id') id: string): void {
+    this.events = this.events.filter((event) => event.id !== Number(id));
+  }
 }
