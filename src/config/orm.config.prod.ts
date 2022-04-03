a7f6ed86d6012ba5d67249108714e4b87bd267c9
Italo Guajardo <italog.penaloza@gmail.com>
2022-04-03 17:13:09 -0400

    Add todo to prod config, IMPORTANT!!

--- src/config/orm.config.prod.ts
+++ src/config/orm.config.prod.ts
@@ -1,6 +1,7 @@
 import { registerAs } from '@nestjs/config';
 import { join } from 'path';
 
+// TODO: Change it to work with @nestjs/mongoose instead of typeorm mondodb
 export default registerAs('orm.config', () => ({
   type: 'mongodb',
   useUnifiedTopology: true,
